bfa9d3fff8756e10c3bee1617d369cc3783566da
tuxman <n.edmonds@topl.me>
2019-06-18 17:32:32 -0500

    Printing global objects from a Graal Context environment

---
+++ src/test/scala/graal/GlobalTests.scala
@@ -0,0 +1,29 @@
+package graal
+
+import org.graalvm.polyglot.{Context, Value}
+import org.scalatest.{Matchers, PropSpec}
+
+/**
+  * GraalJS defines a global 'Graal' object
+  */
+class GlobalTests extends PropSpec with Matchers{
+
+  val context: Context = Context.create("js")
+
+  val script: String =
+    s"""
+       |if (typeof Graal != 'undefined') {
+       |  print(Graal.versionJS);
+       |  print(Graal.versionGraalVM);
+       |  print(Graal.isGraalRuntime);
+       |}
+     """.stripMargin
+
+  val graalOutput: Value = context.eval("js", script)
+
+  println(graalOutput)
+
+  val globalOutput: Value = context.eval("js", "this")
+
+  println(globalOutput)
+}
